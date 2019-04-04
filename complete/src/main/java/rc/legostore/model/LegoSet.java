5089eb3dcefa321b26f3942f8117d552b4051846
Dan Geabunea <dangeabunea@hotmail.com>
2019-04-04 21:26:11 +0300

    Section 3 Completed - Connecting to a Mongo database

--- complete/src/main/java/rc/legostore/model/LegoSet.java
+++ complete/src/main/java/rc/legostore/model/LegoSet.java
@@ -1,15 +1,28 @@
 package rc.legostore.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
+@Document(collection = "legosets")
 public class LegoSet {
+    @Id
     private String id;
     private String name;
     private LegoSetDifficulty difficulty;
+    @Indexed(direction = IndexDirection.ASCENDING)
     private String theme;
     private Collection<ProductReview> reviews = new ArrayList<>();
+
+    @Field("delivery")
     private DeliveryInfo deliveryInfo;
 
     public LegoSet(String name,
@@ -26,7 +39,7 @@ public class LegoSet {
         }
     }
 
-
+    @Transient
     private int nbParts;
 
     public String getId() {
