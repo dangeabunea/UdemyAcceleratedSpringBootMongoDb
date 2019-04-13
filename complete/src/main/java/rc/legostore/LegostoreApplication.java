56009e22fe03de3ad34e1cd98c8669e451643624
Dan Geabunea <dangeabunea@hotmail.com>
2019-04-13 15:31:03 +0300

    Lecture 35 - Configured MongoBee

--- complete/src/main/java/rc/legostore/LegostoreApplication.java
+++ complete/src/main/java/rc/legostore/LegostoreApplication.java
@@ -1,7 +1,11 @@
 package rc.legostore;
 
+import com.github.mongobee.Mongobee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class LegostoreApplication {
@@ -10,4 +14,15 @@ public class LegostoreApplication {
         SpringApplication.run(LegostoreApplication.class, args);
     }
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Bean
+    public Mongobee mongobee() {
+        Mongobee runner = new Mongobee("mongodb://localhost:27017/legostore");
+        runner.setMongoTemplate(mongoTemplate);
+        runner.setChangeLogsScanPackage("rc.legostore.persistence");
+
+        return runner;
+    }
 }
